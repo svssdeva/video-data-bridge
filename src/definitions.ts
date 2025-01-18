cb2d6bcebd22cb11e0abf3a5212fd9c958625e51
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2025-01-18 13:22:04 +0530

    feat: Initial commit

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,14 @@
 export interface VideoDataBridgePlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+    echo(options: { value: string }): Promise<{ value: string }>;
+
+    openVideo(videoData: VideoData): Promise<void>;
+}
+
+export interface VideoData {
+    token: string,
+    userId: string,
+    videoData: any,
+    clientId: string,
+    clientType: string,
+    randomId: string
 }
--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,16 @@
-import { WebPlugin } from '@capacitor/core';
+import {WebPlugin} from '@capacitor/core';
 
-import type { VideoDataBridgePlugin } from './definitions';
+import type {VideoData, VideoDataBridgePlugin} from './definitions';
+import * as console from "node:console";
 
 export class VideoDataBridgeWeb extends WebPlugin implements VideoDataBridgePlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
-  }
+    async echo(options: { value: string }): Promise<{ value: string }> {
+        console.log('ECHO', options);
+        return options;
+    }
+
+    async openVideo(videoData: VideoData): Promise<void> {
+        console.log(videoData);
+        return Promise.resolve(undefined);
+    }
 }
